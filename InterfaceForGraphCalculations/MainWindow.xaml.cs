1cc54e3916f5560cc4e21e73818cffb87c91bc4d
MinistrUtoky <dulov-vitya@mail.ru>
2023-09-17 19:38:37 +0300

    Simple scalable plot canvas

--- InterfaceForGraphCalculations/MainWindow.xaml.cs
+++ InterfaceForGraphCalculations/MainWindow.xaml.cs
@@ -20,9 +20,28 @@ namespace InterfaceForGraphCalculations
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<Point> points = new List<Point>();
+        private List<Line> branches = new List<Line>(); 
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            XAxis.X2 = MainCanvas.ActualWidth;
+            XAxis.Y1 = MainCanvas.ActualHeight - 30; XAxis.Y2 = MainCanvas.ActualHeight - 30;
+            YAxis.Y2 = MainCanvas.ActualHeight - 30;
+            XAxisCoords.Width = MainCanvas.ActualWidth - 30;
+            YAxisCoords.Height = MainCanvas.ActualHeight - 30;
+            Canvas.SetLeft(XAxisCoords, 30);
+            Canvas.SetTop(XAxisCoords, MainCanvas.ActualHeight - 30);
+            Canvas.SetBottom(YAxisCoords, 30);
+        }
+
+        private void MainCanvas_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+
         }
     }
 }
