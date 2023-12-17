020de7444dbf9711d12c78ea16c491d35b71a000
MinistrUtoky <dulov-vitya@mail.ru>
2023-12-17 18:46:02 +0300

    saving whilst creating

--- InterfaceForGraphCalculations/MainWindow.xaml.cs
+++ InterfaceForGraphCalculations/MainWindow.xaml.cs
@@ -809,6 +809,7 @@ namespace InterfaceForGraphCalculations
         {
             GraphNameCSV.Text = "";
             GraphDescriptionCSV.Text = "";
+            NewGraphPopup.IsOpen = false;
             SaveToCSVPopup.IsOpen = true;
         }
         private void SaveToCSVButton_Click(object sender, RoutedEventArgs e)
@@ -1457,8 +1458,7 @@ namespace InterfaceForGraphCalculations
                 path[i].GetVisualVertex().VisualPoint.Stroke = Brushes.Blue;
             }
             InfoField.Text = "Path's max delay: " + mainGraph.GetMaxPathDelay(point1.Vertex, point2.Vertex)
-                                + "\nPath's average delay: " + mainGraph.GetAveragePathDelay(point1.Vertex, point2.Vertex) +
-                                "\nPath's delay: " + mainGraph.GetPathDelay(point1.Vertex, point2.Vertex);
+                                + "\nPath's average delay: " + mainGraph.GetAveragePathDelay(point1.Vertex, point2.Vertex) ;
             RedrawGraph();
         }
         private void StartPathHere_Click(object sender, RoutedEventArgs e)
@@ -1686,8 +1686,7 @@ namespace InterfaceForGraphCalculations
             }
             Edge maxDelayEdge = mainGraph.GetMaxPathDelayEdge(point1.Vertex, point2.Vertex);
             InfoField.Text = "Path's max delay: " + mainGraph.GetMaxPathDelay(point1.Vertex, point2.Vertex)
-                                + "\nPath's average delay: " + mainGraph.GetAveragePathDelay(point1.Vertex, point2.Vertex)+
-                                "\nPath's delay: "+mainGraph.GetPathDelay(point1.Vertex,point2.Vertex);
+                                + "\nPath's average delay: " + mainGraph.GetAveragePathDelay(point1.Vertex, point2.Vertex);
             RedrawGraph();
             maxDelayEdge.GetVisualEdge().VisualBranch.Stroke = Brushes.Violet;
         }
